d89f73c175e7cf22991ec7bf4a7c0c9535aeafa7
Guillermo Tala <guille86598@gmail.com>
2017-06-24 03:51:08 +0000

    triangulo test

--- triangulo.js
+++ triangulo.js
@@ -11,33 +11,40 @@ $(document).ready(function(){
 
 function EvaluarTriangulo(a,b,c)
 {
+    if(isNaN(a) || isNaN(b) || isNaN(c))
+    {
+        $("#lblResultado").text("Sólo se pueden ingresar números").addClass("label label-danger");
+        return;
+    }
+    
     if(!a || !b || !c)
     {
-        $("#lblResultado").text("debe ingresar todos los campos!").addClass("error");
+        $("#lblResultado").text("Debe ingresar todos los campos!").addClass("label label-danger");
         return;
     }
     if(parseInt(a)< 0 || parseInt(b) < 0 || parseInt(c) < 0)
     {
-        $("#lblResultado").text("No se permiten lados negativos").addClass("error");
+        $("#lblResultado").text("No se permiten lados negativos").addClass("label label-danger");
     
         return ;
     }
     
-    $("#lblResultado").removeClass("error");
+    $("#lblResultado").removeClass("label label-danger");
+    $("#lblResultado").addClass("label label-info");
     
     if(a == b && b == c)
     {
-        $("#lblResultado").text("Es equilatero!");
+        $("#lblResultado").text("El triangulo es equilatero!");
     }
     
     if(a != b && b != c && a  != c)
     {
-        $("#lblResultado").text("Es Escaleno!");
+        $("#lblResultado").text("El triangulo es Escaleno!");
     }
     
     if((a == b && a != c) || (a == c && a != b) || (b == c && b != a))
     {
-        $("#lblResultado").text("Es Isoceles!");
+        $("#lblResultado").text("El triangulo es Isoceles!");
     }
 }
 
