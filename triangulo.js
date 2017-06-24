9a65354ea3816a8e6fe42a51c65773f0d0de39f9
Nicolas Gonzalez <nicogon@gmail.com>
2017-06-24 13:34:30 -0300

    agregado framework test y algunos test

--- triangulo.js
+++ triangulo.js
@@ -1,5 +1,53 @@
+const EQUILATERO ="Equilatero";
+const ESCALENO ="Escaleno" ;
+const ISOCELES="Isoceles";
+
+
+
+
 $(document).ready(function(){
     
+
+
+
+QUnit.test("Test de Cumple con tipo Triangulo", function (assert) {
+
+   
+    assert.equal( EvaluarTriangulo(1,1,1),EQUILATERO,"Triangulo es Equilatero");
+    assert.equal( EvaluarTriangulo(2,1,2),ISOCELES,"Triangulo es Isoceles");
+    assert.equal( EvaluarTriangulo(2,1,3),ESCALENO,"Triangulo es Escaleno");
+ 
+});
+
+
+
+
+QUnit.test("Exepciones", function (assert) {
+
+  assert.throws(
+    function() {throw EvaluarTriangulo(1,"a",3);},
+    "Los datos ingresados no son numericos",
+    "Tira exepcion por datos no numericos"
+  );
+ 
+
+   assert.throws(
+    function() {throw EvaluarTriangulo(20,5,5);},
+    "No forma triangulo",
+    "Tira exepcion por no formar triangulo"
+  );
+
+
+
+
+
+
+});
+
+
+
+
+
  $("#btnResultado").click(
      function(){
          EvaluarTriangulo($("#txtLadoA").val(), $("#txtLadoB").val(), $("#txtLadoC").val());
@@ -9,12 +57,15 @@ $(document).ready(function(){
  
 });
 
+
+
 function EvaluarTriangulo(a,b,c)
 {
     if(isNaN(a) || isNaN(b) || isNaN(c))
     {
         $("#lblResultado").text("Sólo se pueden ingresar números").addClass("label label-danger");
-        return;
+       throw "Los datos ingresados no son numericos";
+       return;
     }
     
     if(!a || !b || !c)
@@ -35,16 +86,21 @@ function EvaluarTriangulo(a,b,c)
     if(a == b && b == c)
     {
         $("#lblResultado").text("El triangulo es equilatero!");
+        return EQUILATERO;
     }
     
     if(a != b && b != c && a  != c)
     {
         $("#lblResultado").text("El triangulo es Escaleno!");
+        return ESCALENO;
     }
     
     if((a == b && a != c) || (a == c && a != b) || (b == c && b != a))
     {
         $("#lblResultado").text("El triangulo es Isoceles!");
+        return ISOCELES;
     }
 }
+
+
 
